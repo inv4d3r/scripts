4e0574097d0161ac1ba66f44e0237e17577c5008
Mariusz Zalewski <mariusz.zalewski@tty.host>
2020-04-01 13:56:11 +0200

    add script to create script symbolic links to ~/script directory

---
+++ create_links.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ln -fs "$PWD/"arch_install_packages.sh ~/scripts/arch_install_packages.sh
+ln -fs "$PWD/"autorun.sh ~/scripts/autorun.sh
+ln -fs "$PWD/"colors.sh ~/scripts/colors.sh
+ln -fs "$PWD/"colortrans.py ~/scripts/colortrans.py
+ln -fs "$PWD/"git-make-pull.sh ~/scripts/git-make-pull.sh
+ln -fs "$PWD/"load-ssh.sh ~/scripts/load-ssh.sh
+ln -fs "$PWD/"gkgetsecret.py ~/scripts/gkgetsecret.py
+ln -fs "$PWD/"gkgetsecret.py ~/.offlineimap.py
+ln -fs "$PWD/"run_tmux.sh ~/scripts/run_tmux.sh
+ln -fs "$PWD/"qute-buku.sh ~/scripts/qute-buku.sh
